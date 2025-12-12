c3d46e9fcbb6454bcdfb631104c492e96aa7dc1b
上帝的梦 <1597691532@qq.com>
2025-12-12 14:33:18 +0800

    1

--- index.e8f16.js
+++ index.e8f16.js
@@ -1,4 +1,4 @@
-System.register(["./application.bee3c.js"], function (_export, _context) {
+System.register(["./application.72bb8.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
